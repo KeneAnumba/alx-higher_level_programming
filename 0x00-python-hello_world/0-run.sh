da67821ea81e315fb82a7ca0e54c49480b5b8f07
KeneAnumba <bazone2020@gmail.com>
2024-07-24 15:19:58 -0400

    I updated the descriptions in 0-run and 1-run_inline files

--- 0x00-python-hello_world/0-run.sh
+++ 0x00-python-hello_world/0-run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 #Here, I am required to write a Shell script that runs a Python script
+#The python script's name is learn.py
 #The python file name will be saved in the environment variable $PYFILE
-#The python name is learn.py
+#To export the python code to $PYFILE env variable, I did this:     export PYFILE=learn.py
 
 python -s "$PYFILE"
--- 0x00-python-hello_world/1-run_inline.sh
+++ 0x00-python-hello_world/1-run_inline.sh
@@ -2,7 +2,7 @@
 
 #Here, I am writing a Shell script that runs a python code
 #The python code will be saved in the environment variable $PYCODE
-#To export the python code to $PYCODE env variable, I did  export PYCODE="print('hello world, it is me again')"
+#To export the python code to $PYCODE env variable, I did this:      export PYCODE="print('hello world, it is me again')"
 #Notice how important -c option is here unlike in 0-run.sh. This is because it tells the Python interpreter to execute the code as a string argument. This is useful for running short python commands without needing to create a separate .py file.
 
 python -sc  "$PYCODE"
